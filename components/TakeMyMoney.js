43f0fc0881ea71b825a758ce066e5558ac4fbdde
Andrew Lamansky <andrewlamansky@gmail.com>
2020-04-22 10:55:53 -0400

    fix password reset flow (#1)
    - fix password reset flow
    - TextInput component takes optional prop from parent state
    - conditionally render null in TakeMyMoney if no data

--- components/TakeMyMoney.js
+++ components/TakeMyMoney.js
@@ -50,51 +50,58 @@ class TakeMyMoney extends React.Component {
         token: res.id,
         customerName: res.card.name,
         customerEmail: res.email,
-        customerAddress: `${res.card.address_line1} ${res.card.address_city} ${res.card.address_state} ${res.card.address_zip}`
-      }
-    }).catch(err => {
+        customerAddress: `${res.card.address_line1} ${res.card.address_city} ${res.card.address_state} ${res.card.address_zip}`,
+      },
+    }).catch((err) => {
       alert(err.message);
     });
     toggleCart();
     Router.push({
       pathname: "/order",
-      query: { id: order.data.createOrder.id }
+      query: { id: order.data.createOrder.id },
     });
   };
   render() {
     return (
       <User>
-        {({ data: { me } }) => (
-          <Mutation mutation={TOGGLE_CART_MUTATION}>
-            {toggleCart => {
-              return (
-                <Mutation
-                  mutation={CREATE_ORDER_MUTATION}
-                  refetchQueries={[{ query: CURRENT_USER_QUERY }]}
-                >
-                  {createOrder => (
-                    <StripeCheckout
-                      amount={calcTotalPrice(me.cart)}
-                      name="Beowulf Beard Co"
-                      description={`Order of ${totalItems(me.cart)} items`}
-                      image={
-                        me.cart.length &&
-                        me.cart[0].item &&
-                        me.cart[0].item.image
-                      }
-                      stripeKey="pk_test_cuneRiwfxR18sDgqqv8haHmB00avtCjBnE"
-                      currency="USD"
-                      token={res => this.onToken(res, createOrder, toggleCart)}
-                      shippingAddress
-                    >
-                      {this.props.children}
-                    </StripeCheckout>
-                  )}
-                </Mutation>
-              );
-            }}
-          </Mutation>
-        )}
+        {({ data: { me } = {} } = {}) => {
+          if (!me) {
+            return null;
+          }
+          return (
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {(toggleCart) => {
+                return (
+                  <Mutation
+                    mutation={CREATE_ORDER_MUTATION}
+                    refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+                  >
+                    {(createOrder) => (
+                      <StripeCheckout
+                        amount={calcTotalPrice(me.cart)}
+                        name="Beowulf Beard Co"
+                        description={`Order of ${totalItems(me.cart)} items`}
+                        image={
+                          me.cart.length &&
+                          me.cart[0].item &&
+                          me.cart[0].item.image
+                        }
+                        stripeKey="pk_test_cuneRiwfxR18sDgqqv8haHmB00avtCjBnE"
+                        currency="USD"
+                        token={(res) =>
+                          this.onToken(res, createOrder, toggleCart)
+                        }
+                        shippingAddress
+                      >
+                        {this.props.children}
+                      </StripeCheckout>
+                    )}
+                  </Mutation>
+                );
+              }}
+            </Mutation>
+          );
+        }}
       </User>
     );
   }
