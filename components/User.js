f6aac6bc0ea269d1498e1a4bf862e83694e2bb08
Andrew Lamansky <andrewlamansky@gmail.com>
2020-04-23 15:26:29 -0400

    convert User.js to class-based component with lifecycle hook to stop polling CURRENT_USER query

--- components/User.js
+++ components/User.js
@@ -1,6 +1,7 @@
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import propTypes from "prop-types";
+import React, { Component } from "react";
 
 const CURRENT_USER_QUERY = gql`
   query CURRENT_USER_QUERY {
@@ -22,11 +23,48 @@ const CURRENT_USER_QUERY = gql`
   }
 `;
 
-const User = (props) => (
+/* const User = (props) => (
   <Query {...props} query={CURRENT_USER_QUERY} pollInterval={1500}>
-    {(payload) => props.children(payload)}
+    {(payload) => {
+      let stopPolling = payload.stopPolling;
+      console.log(stopPolling);
+      return props.children(payload);
+    }}
   </Query>
-);
+); */
+
+class User extends Component {
+  state = {
+    _window: false,
+  };
+
+  componentDidMount() {
+    this.setState({ _window: true });
+  }
+
+  killPoll = (stopPolling) => {
+    stopPolling();
+    console.log("got em");
+  };
+
+  setTimer = (stopPolling) => {
+    if (this.state._window) {
+      window.setTimeout(() => this.killPoll(stopPolling), 2500);
+    }
+  };
+  render() {
+    return (
+      <Query {...this.props} query={CURRENT_USER_QUERY} pollInterval={1000}>
+        {(payload) => {
+          let stopPolling = payload.stopPolling;
+          this.state._window && this.setTimer(stopPolling);
+          console.log(stopPolling);
+          return this.props.children(payload);
+        }}
+      </Query>
+    );
+  }
+}
 
 User.propTypes = {
   children: propTypes.func.isRequired,
